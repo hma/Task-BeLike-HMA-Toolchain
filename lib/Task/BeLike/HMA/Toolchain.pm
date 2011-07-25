a6565c7d16f81d68976f6d2efcb9bf15e833b31b
Henning Manske <hma@cpan.org>
2011-07-25 19:24:53 +0200

    Add Archive::Zip, Module::CoreList; remove YAML::Tiny

--- lib/Task/BeLike/HMA/Toolchain.pm
+++ lib/Task/BeLike/HMA/Toolchain.pm
@@ -5,7 +5,7 @@ use 5.006001;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.07_01';
 
 $VERSION = eval $VERSION;
 
@@ -17,7 +17,7 @@ Task::BeLike::HMA::Toolchain - HMA's Perl toolchain update
 
 =head1 VERSION
 
-This is version 0.07 of Task::BeLike::HMA::Toolchain as of Jul 23, 2011.
+This is version 0.07_01 of Task::BeLike::HMA::Toolchain as of Jul 25, 2011.
 
 =head1 SYNOPSIS
 
