3c3cc833d40c5e3816c99adde034a2d7950f5a26
Henning Manske <hma@cpan.org>
2011-08-01 14:34:58 +0200

    Optimize installation order and requirements

--- lib/Task/BeLike/HMA/Toolchain.pm
+++ lib/Task/BeLike/HMA/Toolchain.pm
@@ -5,7 +5,7 @@ use 5.006001;
 use strict;
 use warnings;
 
-our $VERSION = '0.07_01';
+our $VERSION = '0.07_02';
 
 $VERSION = eval $VERSION;
 
@@ -17,7 +17,7 @@ Task::BeLike::HMA::Toolchain - HMA's Perl toolchain update
 
 =head1 VERSION
 
-This is version 0.07_01 of Task::BeLike::HMA::Toolchain as of Jul 25, 2011.
+This is version 0.07_02 of Task::BeLike::HMA::Toolchain as of Aug 01, 2011.
 
 =head1 SYNOPSIS
 
